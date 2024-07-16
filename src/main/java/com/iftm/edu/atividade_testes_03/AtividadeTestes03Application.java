4255e380a97310ed5d16e27b640c62a78d38c3a1
brunovcunha <bruno.cunha@estudante.iftm.edu.br>
2024-07-16 19:53:11 -0300

    inicio

---
+++ src/main/java/com/iftm/edu/atividade_testes_03/AtividadeTestes03Application.java
@@ -0,0 +1,13 @@
+package com.iftm.edu.atividade_testes_03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeTestes03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeTestes03Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/iftm/edu/atividade_testes_03/AtividadeTestes03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iftm.edu.atividade_testes_03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeTestes03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
