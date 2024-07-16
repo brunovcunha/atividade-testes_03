33996e9551e8a0b691c2debb4954718378debf21
João Vitorino (IFTM) <133411181+JoaoVitorinoVN@users.noreply.github.com>
2024-07-16 20:56:49 -0300

    Esqueleto adicionado

---
+++ src/test/java/com/iftm/edu/atividade_testes_03/FuncionarioTerceirizadoTest.java
@@ -0,0 +1,21 @@
+package com.iftm.edu.atividade_testes_03;
+
+public class FuncionarioTerceirizadoTest {
+    public void testarConstrutorEntradaDespesasInvalida() {
+
+    }
+
+    public void testarConstrutorEntradasValida() {
+
+    }
+
+    public void testarModificarDespesasEntradaInvalida() {
+
+    }
+    
+    public void testarModificarDespesasEntradaValida() {
+
+    }
+
+}
+
---
+++ src/test/java/com/iftm/edu/atividade_testes_03/FuncionarioTest.java
@@ -0,0 +1,57 @@
+package com.iftm.edu.atividade_testes_03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class FuncionarioTest {
+    @Test
+    public void testaConstrutorFuncionarioTest() {
+        String nome = "Bruno";
+        int horasTrabalhadas = 10;
+        double valorHora = 13.0;
+
+        Funcionario funcionario = new Funcionario(nome, horasTrabalhadas, valorHora);
+    }
+
+    public void testarConstrutorPagamentoInvalido() {
+
+    }
+
+    public void testarConstrutorEntradaValorHoraInvalida() {
+
+    }
+
+    public void testarConstrutorEntradaHorasInvalida() {
+
+    }
+
+    public void testarConstrutorEntradasValida() {
+
+    }
+    
+    public void testarModificarHorasPagamentoInvalido() {
+        
+    }
+    
+    public void testarModificarHorasEntradaInvalida() {
+
+    }
+    
+    public void testarModificarHorasEntradaValida() {
+
+    }
+    
+    public void testarModificarValorPagamentoInvalido() {
+
+    }
+
+    public void testarModificarValorEntradaInvalida() {
+
+    }
+
+    public void testarModificarValorEntradaValida() {
+
+    }
+}
+
