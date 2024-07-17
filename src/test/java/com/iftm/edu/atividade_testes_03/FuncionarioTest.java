1eeb5c18da377d43628b8ed8854269e6871609ea
brunovcunha <bruno.cunha@estudante.iftm.edu.br>
2024-07-16 21:19:10 -0300

    alguns ajustes

--- src/test/java/com/iftm/edu/atividade_testes_03/FuncionarioTest.java
+++ src/test/java/com/iftm/edu/atividade_testes_03/FuncionarioTest.java
@@ -3,15 +3,18 @@ package com.iftm.edu.atividade_testes_03;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 public class FuncionarioTest {
+   
     @Test
     public void testaConstrutorFuncionarioTest() {
         String nome = "Bruno";
         int horasTrabalhadas = 10;
         double valorHora = 13.0;
 
-        Funcionario funcionario = new Funcionario(nome, horasTrabalhadas, valorHora);
+
     }
 
     public void testarConstrutorPagamentoInvalido() {
