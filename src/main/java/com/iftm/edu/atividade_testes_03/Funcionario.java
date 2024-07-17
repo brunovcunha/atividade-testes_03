c572434469f8dfa26e759ca5b6b1df515fad64b7
João Vitorino V. Neto <133411181+JoaoVitorinoVN@users.noreply.github.com>
2024-07-16 21:52:43 -0300

    Errei o aquivo

--- src/main/java/com/iftm/edu/atividade_testes_03/Funcionario.java
+++ src/main/java/com/iftm/edu/atividade_testes_03/Funcionario.java
@@ -6,31 +6,36 @@ public class Funcionario {
     private int horasTrabalhadas;
     private double valorHora;
 
-
-    public Funcionario(String nome, int horasTrabalhadas, double valorHora) {
+    public String getNome() {
+        return nome;
     }
 
     public int getHorasTrabalhadas() {
         return horasTrabalhadas;
     }
 
-    public void setHorasTrabalhadas(int horasTrabalhadas) {
-        this.horasTrabalhadas = horasTrabalhadas;
-    }
-
-    public String getNome() {
-        return nome;
+    public double getValorHora() {
+        return valorHora;
     }
 
     public void setNome(String nome) {
         this.nome = nome;
     }
 
-    public double getValorHora() {
-        return valorHora;
+    public void setHorasTrabalhadas(int horasTrabalhadas) {
+        this.horasTrabalhadas = horasTrabalhadas;
     }
 
     public void setValorHora(double valorHora) {
+        this.valorHora = valorHora;
+    }
+
+    public Funcionario() {
+    }
+
+    public Funcionario( String nome, int horasTrabalhadas,double valorHora) {
+        this.horasTrabalhadas = horasTrabalhadas;
+        this.nome = nome;
         this.valorHora = valorHora;
     }
 }
--- src/test/java/com/iftm/edu/atividade_testes_03/FuncionarioTest.java
+++ src/test/java/com/iftm/edu/atividade_testes_03/FuncionarioTest.java
@@ -1,17 +1,29 @@
 package com.iftm.edu.atividade_testes_03;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
 public class FuncionarioTest {
+
+    public Funcionario funcionario;
+
+    @BeforeEach
+    public void InstanciarObjetos() {
+        funcionario = new Funcionario();
+    }
+
     @Test
     public void testaConstrutorFuncionarioTest() {
         String nome = "Bruno";
         int horasTrabalhadas = 10;
         double valorHora = 13.0;
 
-        Funcionario funcionario = new Funcionario(nome, horasTrabalhadas, valorHora);
+        funcionario = new Funcionario(nome, horasTrabalhadas, valorHora);
     }
 
     public void testarConstrutorPagamentoInvalido() {
@@ -27,19 +39,52 @@ public class FuncionarioTest {
     }
 
     public void testarConstrutorEntradasValida() {
+        String nome = "Joao";
+        int horasTrabalhadas = 35;
+        double valorHora = 60.0;
 
-    }
-    
-    public void testarModificarHorasPagamentoInvalido() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            funcionario = new Funcionario(nome, horasTrabalhadas, valorHora);
+        });;
         
     }
     
-    public void testarModificarHorasEntradaInvalida() {
+    @Test
+    public void testarModificarHorasPagamentoInvalido() {
+        String nome = "Joao";
+        int horasTrabalhadas = 40;
+        double valorHoraValida = 60.0;
+        funcionario = new Funcionario(nome, horasTrabalhadas, valorHoraValida);
 
+        double valorHoraInvalida = 1.0;
+        assertThrows(IllegalArgumentException.class, () -> {
+            funcionario.setValorHora(valorHoraInvalida);
+        });
     }
     
-    public void testarModificarHorasEntradaValida() {
+    @Test
+    public void testarModificarHorasEntradaInvalida() {
+        String nome = "Joao";
+        int horasTrabalhadasValida = 20;
+        double valorHora = 60.0;
+        funcionario = new Funcionario(nome, horasTrabalhadasValida, valorHora);
 
+        int horasTrabalhadasInvalida = 50;
+        assertThrows(IllegalArgumentException.class, () -> {
+            funcionario.setHorasTrabalhadas(horasTrabalhadasInvalida);
+        });
+    }
+
+    @Test
+    public void testarModificarHorasEntradaValida() {
+        String nome = "Joao";
+        int horasTrabalhadas = 20;
+        double valorHora = 60.0;
+        funcionario = new Funcionario(nome, horasTrabalhadas, valorHora);
+
+        int horasTrabalhadasModificada = 30;
+        int resultadoObtido = funcionario.getHorasTrabalhadas();
+        assertEquals(horasTrabalhadasModificada, resultadoObtido);
     }
     
     public void testarModificarValorPagamentoInvalido() {
