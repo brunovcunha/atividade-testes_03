6ad9cd9c79999bb3b8fc5944eef9d3d6069e7804
Vinicius Raphael <133443384+Vini-IFTM@users.noreply.github.com>
2024-07-25 21:23:07 -0300

    Add FuncionarioTercerizado
    Inclusão arquivo FuncionarioTercerizado.java

---
+++ src/main/java/com/iftm/edu/atividade_testes_03/FuncionarioTerceirizado.java
@@ -0,0 +1,22 @@
+package com.iftm.edu.atividade_testes_03;
+
+public class FuncionarioTerceirizado extends Funcionario {
+
+    private double despesas;
+
+    public FuncionarioTerceirizado(String nome, int horasTrabalhadas, double valorHora, double despesas) {
+        super(nome, horasTrabalhadas, valorHora);
+        setDespesas(despesas);
+    }
+
+    public double getDespesas() {
+        return despesas;
+    }
+
+    public void setDespesas(double despesas) {
+        if (despesas < 0) {
+            throw new IllegalArgumentException("Despesas não podem ser negativas.");
+        }
+        this.despesas = despesas;
+    }
+}
